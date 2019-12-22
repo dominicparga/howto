be7d59d61007598c7432b3ec4fbcd6145732f28f
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2019-12-22 18:12:42 +0100

    Edit .gitignore a little

---
+++ scripts/create_gitignore.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+echo '#------------------------------------------------------------------------------#'
+echo '# gitignore'
+curl -L -s 'https://www.gitignore.io/api/macos,visualstudiocode'
+echo ''
+echo '#------------------------------------------------------------------------------#'
+echo '# custom'
+echo ''
+echo '/custom/'
+echo ''
+echo '.vscode/'
