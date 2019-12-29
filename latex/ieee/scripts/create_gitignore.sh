c32b8605ed736c0ef63f2cd04014a035de867710
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2019-12-29 18:47:00 +0100

    Refactor gitignore-creation a little

---
+++ latex/ieee/scripts/create_gitignore.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+echo '#------------------------------------------------------------------------------#'
+echo '# gitignore'
+curl -L -s 'https://www.gitignore.io/api/linux,macos,windows,visualstudiocode,tex,latex'
+echo ''
+echo '#------------------------------------------------------------------------------#'
+echo '# custom'
+echo ''
+echo '/custom/'
+echo ''
+echo '.vscode/'
